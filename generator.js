40004cc7a2a2fcab00ef90c41908f07f30cf7c9f
EugeneBakin <bakin.eugene@ya.ru>
2015-05-29 18:01:25 +0500

    Copying all files to different subfolders and modifying each html file afterwards
    To get support for external css and js

--- generator.js
+++ generator.js
@@ -1,39 +1,88 @@
-var fs = require( 'fs' );
+#!/usr/bin/env node
+
+(function () {
+
+	var TEMPLATES_DIR = 'templates';
+	var DESTINATION_DIR = 'build';
+	var JSON_DIR = 'resume';
+
+	var EXTENSION_HTML = '.html';
+	var EXTENSION_JSON = '.json';
+
+	var fse = require( 'fs-extra' );
+  var pathExt = require('path');
+	var Mark = require( "markup-js" );
+
+	function generateResumes () {
+		walk( JSON_DIR, function (item, path, fullPath) {
+			if ( pathExt.extname(item) === EXTENSION_JSON ) {
+				fse.readFile( fullPath, 'utf8', function ( err, data )
+				{
+					if ( err ) {
+						console.log( err );
+					} else {
+						fse.copy( TEMPLATES_DIR, DESTINATION_DIR + '/' + pathExt.basename( item, EXTENSION_JSON ), function () {
+							generateResume( pathExt.basename( item, EXTENSION_JSON ), DESTINATION_DIR + '/' + pathExt.basename( item, EXTENSION_JSON ), JSON.parse( data ) );
+						});
+					}
+				})
+			}
+		});
+	}
+
+
+	function generateResume ( resumeName, resumeDir, resumeData ) {
+		walk( resumeDir, function(item, path, fullPath) {
+			if ( pathExt.extname(item) === EXTENSION_HTML ) {			
+				fse.readFile( fullPath, 'utf8', function ( err, template ) {
+					if ( err ) {
+						console.log ( err );
+					} else {
+						Mark.globals = resumeData.globals;
+
+						fse.writeFile( fullPath, Mark.up( template, resumeData ), function ( err )	{
+							if ( err ) {
+								console.log( err );
+							} else {
+								console.log( "The file " + resumeName + "  was saved in " + resumeDir );
+							} 
+						});
+					}
+				});
+			}
+		});
+	}
+
+	fse.emptyDir( DESTINATION_DIR , generateResumes);
+
+	function walk(path, callback) {
+		fse.readdir(path, function (err, files) {
+
+			if(err) {
+				console.log(err);
+			} else {
+				files.forEach(function(item) {
+					var filePath = path + '/' + item;
+					fse.stat(filePath, function (err, stats) {
+					
+						if (stats.isDirectory()) {
+							walk(filePath, callback);
+						}
+
+						if (stats.isFile()) {
+							callback(item, path, filePath);
+						}
+					
+					});
+				});
+			}
+		})
+	}
+
+
 var jsonName = process.argv[2];
 var templateName = process.argv[3];
 if ( jsonName && templateName )
 {
-	fs.readFile( jsonName, 'utf8', function ( err, data )
-	{
-		if ( !err )
-		{
-			fs.readFile( templateName, 'utf8', function ( err, template )
-			{
-
-				if ( !err )
-				{
-
-					dataObject = JSON.parse( data );
-
-					var Mark = require( "markup-js" );
-
-					Mark.globals = dataObject.globals;
-
-					var result = Mark.up( template, dataObject );
-
-					fs.writeFile( "result.html", result, function ( err )
-					{
-						if ( err )
-						{
-							return console.log( err );
-						}
-
-						console.log( "The file was saved!" );
-						process.exit();
-					} );
-				}
-			} )
-		}
-	} );
 }
-
+} ());
